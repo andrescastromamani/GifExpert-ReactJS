e6991f21498d70da5e4ab93bcc38a530c81b8047
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-09-08 11:22:16 -0400

    Componente addCategory

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
 
 export const GifExpertApp = () => {
+    const [categories, setcategories] = useState(['anime', 'series', 'comics'])
+    const handleAdd = () => {
+        //setcategories([...categories, 'new category']);
+        setcategories(cats => [...cats, 'new category']);
+    }
     return (
         <>
             <h1>GifExpertApp</h1>
+            <AddCategory />
             <hr></hr>
+
+            <ol>
+                {
+                    categories.map(category => {
+                        return < li key={category} > {category}</li>
+                    })
+                }
+            </ol>
         </>
     )
 }
---
+++ src/components/AddCategory.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+
+export const AddCategory = () => {
+    const [inputValue, setInputValue] = useState('Hola Mundo');
+    const handleAddCategory = (e) => {
+        setInputValue(e.target.value);
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(inputValue);
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder={inputValue}
+                onChange={handleAddCategory} />
+        </form>
+    )
+}
